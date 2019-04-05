31372a7b1c4841ab5f0c0d38c7402dace228134a
lenileiro <lenileiro@gmail.com>
2019-04-05 17:23:44 +0300

    chore(refactor): added client config

--- webpack.config.js
+++ webpack.config.js
@@ -53,28 +53,30 @@ var serverConfig = {
   ]
 }
 
-var developClient = {
-  entry: './client/index.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
+var developClient = (env) => {
+  return {
+    entry: './client/index.js',
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
 
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
+    module: {
+      rules: [
+        { test: /\.(js)$/, use: 'babel-loader' },
+      ]
+    },
+
+    plugins: [
+      htmlplugin,
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+        'process.env.CLIENT': JSON.stringify(env.CLIENT)
+      })
     ]
-  },
-
-  plugins: [
-    htmlplugin,
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
-      'process.env.CLIENT': JSON.stringify(env.CLIENT)
-    })
-  ]
+  }
 }
 
 let webpackConfig = (env) => {
@@ -82,7 +84,7 @@ let webpackConfig = (env) => {
   var client = process.env.CLIENT || "development";
 
   if(client == 'development') {
-    return developClient
+    return developClient(env)
   }
   else if(client == 'build') {
     return browserConfig
